fa571f46c3060f1adb63d11261a4684dcc15531d
Shawn Watson <shawnw86@gmail.com>
2015-11-27 16:22:07 -0800

    Add recommendations

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -1,4 +1,4 @@
 post '/recommendations' do 
-	movie = Movie.find_or_create_by(title: params[:title])
+	movie = Movie.find_or_create_by(title: params[:title].to_s)
 	movie.recommendations.create(recommended: params[:type])
 end
